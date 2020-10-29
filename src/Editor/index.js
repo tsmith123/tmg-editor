4eba621fed523093d165f28ad6da7575083f31e3
Thomas Smith <tomdsmith1@gmail.com>
2020-10-29 14:09:27 +0000

    Add link package to editor

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -11,6 +11,7 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
 import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle'
+import Link from '@ckeditor/ckeditor5-link/src/link'
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
 
@@ -21,8 +22,8 @@ const Editor = ({ onChange, data, placeholder }) => {
       data={data}
       config={{
         placeholder,
-        plugins: [BlockQuote, ListStyle, PasteFromOffice, Heading, Essentials, Paragraph, Bold, Italic, Strikethrough],
-        toolbar: ['heading', '|', 'bold', 'italic', 'strikethrough', '|', 'bulletedList', 'numberedList', 'blockQuote', '|', 'undo', 'redo']
+        plugins: [BlockQuote, ListStyle, PasteFromOffice, Heading, Essentials, Paragraph, Bold, Italic, Strikethrough, Link],
+        toolbar: ['heading', '|', 'bold', 'italic', 'strikethrough', '|', 'link', 'blockQuote', '|', 'bulletedList', 'numberedList', '|', 'undo', 'redo']
       }}
       onChange={(event, editor) => {
         const data = editor.getData()
