f068fba0cbeb2c9d1928d7f77b4538080ea8f9c4
Thomas Smith <tomdsmith1@gmail.com>
2020-10-29 15:22:26 +0000

    Add autolink

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -12,7 +12,7 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
 import List from '@ckeditor/ckeditor5-list/src/list'
 import Link from '@ckeditor/ckeditor5-link/src/link'
-// import AutoLink from '@ckeditor/ckeditor5-link/src/autolink'
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink'
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
 
@@ -23,7 +23,7 @@ const Editor = ({ onChange, data, placeholder }) => {
       data={data}
       config={{
         placeholder,
-        plugins: [Link, BlockQuote, List, PasteFromOffice, Heading, Essentials, Paragraph, Bold, Italic, Strikethrough],
+        plugins: [Link, AutoLink, BlockQuote, List, PasteFromOffice, Heading, Essentials, Paragraph, Bold, Italic, Strikethrough],
         toolbar: ['heading', '|', 'bold', 'italic', 'strikethrough', '|', 'link', 'blockQuote', '|', 'bulletedList', 'numberedList', '|', 'undo', 'redo']
       }}
       onChange={(event, editor) => {
