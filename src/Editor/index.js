a5ab094a33a8d341cf5f319663f4bcf8b0f61da5
Thomas Smith <tomdsmith1@gmail.com>
2020-10-29 15:15:52 +0000

    Use list

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -10,8 +10,8 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
-import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle'
-import Link from '@ckeditor/ckeditor5-link/src/link'
+import List from '@ckeditor/ckeditor5-list/src/list'
+// import Link from '@ckeditor/ckeditor5-link/src/link'
 // import AutoLink from '@ckeditor/ckeditor5-link/src/autolink'
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
@@ -23,7 +23,7 @@ const Editor = ({ onChange, data, placeholder }) => {
       data={data}
       config={{
         placeholder,
-        plugins: [Link, BlockQuote, ListStyle, PasteFromOffice, Heading, Essentials, Paragraph, Bold, Italic, Strikethrough],
+        plugins: [BlockQuote, List, PasteFromOffice, Heading, Essentials, Paragraph, Bold, Italic, Strikethrough],
         toolbar: ['heading', '|', 'bold', 'italic', 'strikethrough', '|', 'link', 'blockQuote', '|', 'bulletedList', 'numberedList', '|', 'undo', 'redo']
       }}
       onChange={(event, editor) => {
