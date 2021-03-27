10af7c6cfaf3728b04de889453839377f006ea3a
Thomas Smith <tomdsmith1@gmail.com>
2021-03-27 11:55:36 +0000

    Revert webpack external changes

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,18 @@ module.exports = {
   plugins: [new CompressionPlugin()],
   externals: {
     // Don't bundle react or react-dom
-    react: 'react',
-    'react-dom': 'react-dom'
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    }
   },
   module: {
     rules: [
