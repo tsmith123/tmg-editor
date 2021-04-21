04310ccbc6b6d0780be14c8dee80181ec54cf8a3
Thomas Smith <tomdsmith1@gmail.com>
2021-04-21 12:45:23 +0100

    Use nextSibling

--- src/plugins/paste/paste.js
+++ src/plugins/paste/paste.js
@@ -18,35 +18,22 @@ export default class Paste extends Plugin {
     editor.plugins.get('ClipboardPipeline').on(
       'inputTransformation',
       (evt, data) => {
-        console.log('Data', data)
         console.log('Content', data.content)
         const writer = new UpcastWriter(viewDocument)
 
         const children = data.content.getChildren()
-        console.log('Children', children)
 
-        for (let i = 0; i < children.length; i++) {
-          const isLastChild = i === children.length - 1
-          const child = children[i]
-          const nextChild = children[i + 1]
-
-          const isMidParagraphBreak = child.is('element', 'br') && !isLastChild && !nextChild.is('element', 'br')
+        for (const child of children) {
+          const isMidParagraphBreak = child.is('element', 'br')
           console.log(isMidParagraphBreak)
 
           const childIndex = data.content.getChildIndex(child)
 
           writer.remove(child)
           writer.insertChild(childIndex, child.getChildren(), data.content)
+
+          console.log(child.nextSibling)
         }
-
-        // for (const child of children) {
-        // if (child.is('element', 'b') && child.getStyle('font-weight') === 'normal') {
-        //   const childIndex = data.content.getChildIndex(child)
-
-        //   writer.remove(child)
-        //   writer.insertChild(childIndex, child.getChildren(), documentFragment)
-        // }
-        // }
       },
       { priority: 'high' }
     )
